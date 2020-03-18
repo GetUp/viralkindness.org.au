9583394084e9f6f348c1d96d130507cfa6f13e85
nirentuladhar <nirentuladhar@gmail.com>
2020-03-19 10:02:15 +1100

    Styles for about, contact, resources

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import PageHeader from '../components/PageHeader'
+import ContentWithSidebar from '../components/ContentWithSidebar'
 
 export default () => (
   <>
     <div>
-      <h1>Contact Us</h1>
-      <p style={{ backgroundColor: '#ff00f7' }}>
-        Having trouble joining a group? Spotted something that we could make
-        better? Let us know!
-      </p>
+      <PageHeader>
+        <h1>Contact Us</h1>
+      </PageHeader>
+      <ContentWithSidebar>
+        <div>
+          <p>
+            Having trouble joining a group? Spotted something that we could make
+            better? Let us know!
+          </p>
 
-      <p style={{ backgroundColor: '#ff00f7' }}>
-        <a href='mailto:viralkindness@getup.org.au'>
-          Email us at viralkindness@getup.org.au
-        </a>{' '}
-        and we’ll get back to you shortly.
-      </p>
+          <p>
+            <a href='mailto:viralkindness@getup.org.au'>
+              Email us at viralkindness@getup.org.au
+            </a>{' '}
+            and we’ll get back to you shortly.
+          </p>
+        </div>
+      </ContentWithSidebar>
     </div>
     <Footer />
   </>
