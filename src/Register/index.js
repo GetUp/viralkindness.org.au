3278728bdbdeeebd0f9407bca6e0f19cf5b88ba3
Tim McEwan <tim@mcewan.it>
2020-03-18 12:49:37 +1100

    Location search with GeoJSON upload

--- src/Register/index.js
+++ src/Register/index.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { v4 as uuidv4 } from 'uuid'
 import { omit } from 'lodash'
 import { useHistory } from 'react-router-dom'
+import Geosuggest from 'react-geosuggest'
 import postJson from '../postJson'
 import s from './index.module.scss'
 import ContentWithSidebar from '../components/ContentWithSidebar'
 import { Private, Public } from '../components/Icons'
 import InputWithIcon from '../components/InputWithIcon'
+import './geosuggest.css'
 
 const apiHost =
   'https://68545911-1f96-432f-809a-c20fb3cf240b-bluemix.cloudant.com'
@@ -17,7 +19,7 @@ const initialValues = {
   phone: '',
   groupName: 'example group',
   groupLink: 'https://m.me/groups',
-  location: 'dummy location',
+  location: '',
   links: '',
   general: ''
 }
@@ -27,34 +29,48 @@ const groupLinkPattern = new RegExp(
   'i'
 )
 
-const validate = values => {
-  const errors = {}
-
-  if (!values.email) {
-    errors.email = 'This field is required'
-  } else if (!/^.+@.+$/i.test(values.email)) {
-    errors.email = "The email address format doesn't look right"
-  }
-
-  if (!values.groupName) {
-    errors.groupName = 'This field is required'
-  }
-
-  if (!values.groupLink) {
-    errors.groupLink = 'This field is required'
-  } else if (!groupLinkPattern.test(values.groupLink)) {
-    errors.groupLink =
-      'The link should be to a Facebook group or group chat on FB Messenger or WhatsApp'
-  }
-
-  return errors
-}
+const geoJson = loc => ({
+  geometry: {
+    coordinates: [loc.location.lng, loc.location.lat],
+    type: 'Point'
+  },
+  properties: loc,
+  type: 'Feature'
+})
 
 export default () => {
   const history = useHistory()
+  const [location, setLocation] = useState({})
+
+  const validate = values => {
+    const errors = {}
+
+    if (!values.email) {
+      errors.email = 'This field is required'
+    } else if (!/^.+@.+$/i.test(values.email)) {
+      errors.email = "The email address format doesn't look right"
+    }
+
+    if (!values.groupName) {
+      errors.groupName = 'This field is required'
+    }
+
+    if (!(location && location.location)) {
+      errors.location = 'A location is needed so people can find the group'
+    }
+
+    if (!values.groupLink) {
+      errors.groupLink = 'This field is required'
+    } else if (!groupLinkPattern.test(values.groupLink)) {
+      errors.groupLink =
+        'The link should be to a Facebook group or group chat on FB Messenger or WhatsApp'
+    }
+
+    return errors
+  }
 
   const onSubmit = (values, { setSubmitting, setFieldError }) => {
-    const payload = { _id: uuidv4(), ...values }
+    const payload = { _id: uuidv4(), ...values, ...geoJson(location) }
     const privateSubmission = postJson(`${apiHost}/viral-kindness/`, payload)
     const publicSubmission = postJson(
       `${apiHost}/viral-kindness-public/`,
@@ -130,7 +146,14 @@ export default () => {
                   Location
                   <InputWithIcon className={s.inputWrapper}>
                     <Public />
-                    <Field type='text' name='location' required />
+                    <Geosuggest
+                      placeholder='Enter your street or suburb'
+                      country='AU'
+                      placeDetailFields={[]}
+                      minLength={4}
+                      onSuggestSelect={setLocation}
+                    />
+                    <ErrorMessage name='location' component='div' />
                   </InputWithIcon>
                 </label>
                 <ErrorMessage name='location' component='div' />
