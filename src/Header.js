edd6d9354d00c173a3855f07376cb6c49d1b839b
Tim McEwan <tim@mcewan.it>
2020-03-19 09:39:03 +1100

    Update header text for consistency

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default () => (
   <header>
     <Nav />
     <Warning href='https://www.getup.org.au/covid19'>
-      COVID-19: What you need to do
+      Coronavirus (COVID-19): What you need to do
     </Warning>
   </header>
 )
