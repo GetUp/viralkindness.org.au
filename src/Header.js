c09036672b9b0edb19eb742e8e797720a7be3607
nirentuladhar <nirentuladhar@gmail.com>
2020-03-19 12:01:15 +1100

    Moved nav below

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ import Warning from './components/Warning'
 
 export default () => (
   <header>
-    <Nav />
     <Warning href='https://www.getup.org.au/covid19'>
       Coronavirus (COVID-19): What you need to do
     </Warning>
+    <Nav />
   </header>
 )
