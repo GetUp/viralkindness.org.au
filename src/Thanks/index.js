963f33f17bb77dfc4f22531809e6da7f8be3fc4e
nirentuladhar <nirentuladhar@gmail.com>
2020-03-19 11:30:19 +1100

    Add share module

--- src/Thanks/index.js
+++ src/Thanks/index.js
@@ -1,26 +1,35 @@
 import React from 'react'
+import s from './index.module.scss'
 import Footer from '../components/Footer'
+import PageHeader from '../components/PageHeader'
+import ContentWithSidebar from '../components/ContentWithSidebar'
+import ShareModule from '../components/ShareModule'
 
 export default () => (
   <>
     <div>
-      <h1>Thanks for registering your group!</h1>
-      <p style={{ backgroundColor: '#ff00f7' }}>
-        Thanks for starting a community care group in your local area.
-      </p>
-      <p style={{ backgroundColor: '#ff00f7' }}>
-        Without question, the way we pull through coronavirus is together and by
-        caring for those around us.
-        <strong>
-          To do that, we’re going to need everyone who can, to do a little more
-          to help.
-        </strong>
-      </p>
-      <p style={{ backgroundColor: '#ff00f7' }}>
-        Can you share your group with family, friends and neighbours and
-        encourage them to join you?
-      </p>
-      <h1 style={{ backgroundColor: '#ff00f7' }}>SHARE MODULE</h1>
+      <PageHeader>
+        <h1>Thanks for registering your group!</h1>
+      </PageHeader>
+      <ContentWithSidebar className={s.contentContainer}>
+        <div>
+          <p>Thanks for starting a community care group in your local area.</p>
+          <p>
+            Without question, the way we pull through coronavirus is together
+            and by caring for those around us.
+            <strong>
+              To do that, we’re going to need everyone who can, to do a little
+              more to help.
+            </strong>
+          </p>
+          <p>
+            Can you share your group with family, friends and neighbours and
+            encourage them to join you?
+          </p>
+        </div>
+
+        <ShareModule className={s.shareModule} />
+      </ContentWithSidebar>
     </div>
     <Footer />
   </>
