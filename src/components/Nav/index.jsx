d5767cc7255a4eabb88b96f710b4d0d7bc67bccc
nirentuladhar <nirentuladhar@gmail.com>
2020-03-19 13:53:58 +1100

    Add powered by

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 import s from './index.module.scss'
 import { VKLogo } from '../Icons'
+import GetUp from '../../assets/images/getup-logo.svg'
 
 export default () => (
   <nav className={s.navContainer}>
     <Link to='/' className={s.logoWrapper}>
       <VKLogo />
     </Link>
+    <div className={s.poweredBy}>
+      <div className={s.poweredByText}>Powered By</div>
+      <img src={GetUp} />
+    </div>
   </nav>
 )
