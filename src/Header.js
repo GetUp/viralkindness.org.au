b04e570c8e358787e74ac16593d7052061545e74
Tim McEwan <tim@mcewan.it>
2020-03-19 13:09:40 +1100

    Update banner text

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import Warning from './components/Warning'
 export default () => (
   <header>
     <Warning href='https://www.getup.org.au/covid19'>
-      Coronavirus (COVID-19): What you need to do
+      Coronavirus (COVID-19): What you can do
     </Warning>
     <Nav />
   </header>
