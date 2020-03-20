2274c7ccb3bdb5493094ada49456487201b850c0
Tim McEwan <tim@mcewan.it>
2020-03-20 16:51:10 +1100

    Map loaded before polyfills; causing errors in IE

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import 'react-app-polyfill/ie11'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import ReactGoogleMapLoader from 'react-google-maps-loader'
 import App from './App'
 import smoothscroll from 'smoothscroll-polyfill'
 
 // needed for safari
 smoothscroll.polyfill()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const mapParams = {
+  key: 'AIzaSyDSqqjcb_6Xfrdj75v0a9wT-EGBTBhgxow',
+  libraries: 'places'
+}
+
+ReactDOM.render(
+  <ReactGoogleMapLoader
+    params={mapParams}
+    render={mapLoaded => mapLoaded && <App />}
+  />,
+  document.getElementById('root')
+)
