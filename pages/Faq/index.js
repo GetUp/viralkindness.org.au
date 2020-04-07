2c8555fdc3ccf4dc31b0973d34179516c3e0f976
nirentuladhar <nirentuladhar@gmail.com>
2020-04-07 12:09:45 +1000

    convert faq to md

--- pages/Faq/index.js
+++ pages/Faq/index.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react'
 import ReactGA from 'react-ga'
 import ContentWithSidebar from '../../components/ContentWithSidebar'
 import FaqNav from '../../components/FaqNav'
-import faq from '../../components/data/faq'
 import PageHeader from '../../components/PageHeader'
 import s from './index.module.scss'
+import { attributes as faq } from '../../content/faq.md'
 
 const Item = i => (
   <div key={i.title} className={s.question}>
@@ -25,14 +25,11 @@ export default () => {
     <>
       <PageHeader>
         <h1 className={s.pageHeader}>{faq.title}</h1>
-        <p>
-          As more groups offer advice on what has worked well for them, we will
-          update this FAQ page.
-        </p>
+        <p>{faq.subtitle}</p>
       </PageHeader>
       <ContentWithSidebar reverse className={s.contentWrapper}>
         <div style={{ maxWidth: '750px' }} className={s.content}>
-          {faq.data.map(Item)}
+          {faq.questions.map(Item)}
         </div>
         <FaqNav className={s.faqWrapper} />
       </ContentWithSidebar>
