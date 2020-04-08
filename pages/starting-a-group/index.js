9ec5ee0e153c736464a775db49a5c288313cb2cf
nirentuladhar <nirentuladhar@gmail.com>
2020-04-08 13:07:27 +1000

    add starting a group page

---
+++ pages/starting-a-group/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import ReactGA from 'react-ga'
+import ContentWithSidebar from '../../components/ContentWithSidebar'
+import FaqNav from '../../components/FaqNav'
+import PageHeader from '../../components/PageHeader'
+import s from './index.module.scss'
+import { attributes as c } from '../../content/starting-a-group.md'
+
+const Item = i => (
+  <div key={i.title} className={s.question}>
+    <h2 id={i.hash} className={s.header}>
+      {i.title}
+    </h2>
+    <div dangerouslySetInnerHTML={{ __html: i.text }} className={s.body} />
+  </div>
+)
+
+export default () => {
+  useEffect(
+    () => ReactGA.pageview(window.location.pathname + window.location.search),
+    []
+  )
+
+  return (
+    <>
+      <PageHeader>
+        <h1 className={s.pageHeader}>{c.title}</h1>
+        <p>{c.subtitle}</p>
+      </PageHeader>
+      <ContentWithSidebar reverse className={s.contentWrapper}>
+        <div style={{ maxWidth: '750px' }} className={s.content}>
+          {c.steps.map(Item)}
+        </div>
+      </ContentWithSidebar>
+    </>
+  )
+}
