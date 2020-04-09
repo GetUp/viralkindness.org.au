dd7766445ae5892588fade169d9d63bc27078bd8
nirentuladhar <nirentuladhar@gmail.com>
2020-04-09 14:18:07 +1000

    Return a tag for mailto

--- components/Markdown/index.jsx
+++ components/Markdown/index.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 import Markdown from 'markdown-to-jsx'
 import Link from 'next/link'
 
-const NextLink = ({ children, ...props }) => (
-  <Link {...props}>
-    <a>{children}</a>
-  </Link>
-)
+const NextLink = ({ children, className = '', href, ...props }) => {
+  const mailto = /mailto|http/
+  const http = /http/
+
+  return (
+    <>
+      {mailto.test(href) ? (
+        <a href={href} target={http.test(href) && '_blank'} {...props}>
+          {children}
+        </a>
+      ) : (
+        <Link href={href}>
+          <a className={className} {...props}>
+            {children}
+          </a>
+        </Link>
+      )}
+    </>
+  )
+}
 
 export default ({ children }) => {
   return (
